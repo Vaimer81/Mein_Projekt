dcbd231ac87d9bb48351cf6c02dafb40789348fd
Vaimer <vitaliivaimer83@gmail.com>
2023-09-09 23:22:03 +0200

    стили

--- app/src/components/Footer/index.jsx
+++ app/src/components/Footer/index.jsx
@@ -4,8 +4,10 @@ import GoogleMap from './GoogleMap'
 
 export default function Footer() {
   return (
-    <div className={s.footer}>
-     <GoogleMap/>
+    <div className={s.google}>
+		  <GoogleMap />
+		<div className={s.h5}>  <h6>The Best Shop For The Best goods (c)</h6>
+      <h6>2023</h6> </div>
     </div>
   )
 }
